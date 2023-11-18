29f4c6873a7ddf2570764d7f4c36671936ddbcbb
Abdurahman <raimberdi.uulu.abdurahman@gmail.com>
2023-11-19 01:01:19 +0600

    HW 2

---
+++ post/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    image = models.ImageField(upload_to = 'product', null = True, blank = True)
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    rate = models.FloatField(default = 0)
+    create_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
---
+++ post/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from post.models import Product
+
+
+
+
+def title_page(request):
+    if request.method == 'GET':
+        Product.objects.all()
+        return render(request, 'index.html')
+
+
+
+def product_view(request):
+     if request.method == 'GET':
+        products = Product.objects.all()
+        context = {
+            'products': products
+        }
+
+        return render(request, 'product.html', context)
+
+
+
+
+# def hello_world(request):
+#     #return HttpResponse("Hello Bruh! What's up ?")
+#     if request.method == 'GET':
+#         return render(request, 'hello.html')
+
+
+# def current_date(request):
+#     now = datetime.now()
+#     context = {
+#         'current_date': now.strftime("%Y-%m-%d"),
+#         'current_time': now.strftime("%H:%M:%S"),
+#     }
+#     if request.method == 'GET':
+#         return render(request, 'current_date.html', context)
+
+
+# def goodby (request):
+#     if request.method == 'GET':
+#         return render(request, 'goodby.html')
